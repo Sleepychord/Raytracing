f087cc4f01f980c89e72ce904d4759ff083e33e6
Sleepy_chord <sj702d5@foxmail.com>
2016-06-19 19:34:52 +0800

    sample

--- src/photon_mapper.cpp
+++ src/photon_mapper.cpp
@@ -8,7 +8,7 @@ PhotonMapper::PhotonMapper(std::string filename)
 }
 void PhotonMapper::buildHitMap(){
     srand(time(0));
-    const int SAMPLENUM = 4;
+    const int SAMPLENUM = 16;
     vector<thread> thpool(SAMPLENUM);
     vector<long long> seed;
     for(int i = 0;i < SAMPLENUM;i++)
